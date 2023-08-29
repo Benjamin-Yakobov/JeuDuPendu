ef52dbc5b8a55afb5cebfce5db6147993f3b8246
Yakobov-Benjamin <yakobovbenjamin@gmail.com>
2023-08-29 17:57:24 +0200

    Sprint 2 : Gérer la navigation et la sécurisation de l'interface.

--- JeuDuPendu/JeuDuPendu/FrmPendu.Designer.cs
+++ JeuDuPendu/JeuDuPendu/FrmPendu.Designer.cs
@@ -37,6 +37,7 @@ namespace JeuDuPendu
             this.cmbLettres = new System.Windows.Forms.ComboBox();
             this.pctPendu = new System.Windows.Forms.PictureBox();
             this.btnRejouer = new System.Windows.Forms.Button();
+            this.lblResultat = new System.Windows.Forms.Label();
             this.grpMot.SuspendLayout();
             this.grpTest.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pctPendu)).BeginInit();
@@ -45,28 +46,29 @@ namespace JeuDuPendu
             // grpMot
             // 
             this.grpMot.Controls.Add(this.txtMot);
-            this.grpMot.Location = new System.Drawing.Point(99, 92);
+            this.grpMot.Location = new System.Drawing.Point(115, 106);
             this.grpMot.Name = "grpMot";
-            this.grpMot.Size = new System.Drawing.Size(324, 52);
+            this.grpMot.Size = new System.Drawing.Size(378, 60);
             this.grpMot.TabIndex = 0;
             this.grpMot.TabStop = false;
             this.grpMot.Text = "Mot a chercher";
             // 
             // txtMot
             // 
-            this.txtMot.Location = new System.Drawing.Point(56, 19);
+            this.txtMot.Location = new System.Drawing.Point(65, 22);
             this.txtMot.Name = "txtMot";
-            this.txtMot.Size = new System.Drawing.Size(203, 20);
+            this.txtMot.Size = new System.Drawing.Size(236, 21);
             this.txtMot.TabIndex = 0;
+            this.txtMot.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtMot_KeyPress);
             // 
             // grpTest
             // 
             this.grpTest.Controls.Add(this.lblLettres);
             this.grpTest.Controls.Add(this.btnTest);
             this.grpTest.Controls.Add(this.cmbLettres);
-            this.grpTest.Location = new System.Drawing.Point(99, 150);
+            this.grpTest.Location = new System.Drawing.Point(115, 173);
             this.grpTest.Name = "grpTest";
-            this.grpTest.Size = new System.Drawing.Size(329, 100);
+            this.grpTest.Size = new System.Drawing.Size(384, 115);
             this.grpTest.TabIndex = 1;
             this.grpTest.TabStop = false;
             this.grpTest.Text = "Test";
@@ -75,7 +77,7 @@ namespace JeuDuPendu
             // 
             this.lblLettres.AutoSize = true;
             this.lblLettres.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblLettres.Location = new System.Drawing.Point(7, 60);
+            this.lblLettres.Location = new System.Drawing.Point(8, 69);
             this.lblLettres.Name = "lblLettres";
             this.lblLettres.Size = new System.Drawing.Size(317, 16);
             this.lblLettres.TabIndex = 2;
@@ -83,47 +85,62 @@ namespace JeuDuPendu
             // 
             // btnTest
             // 
-            this.btnTest.Location = new System.Drawing.Point(184, 19);
+            this.btnTest.Location = new System.Drawing.Point(215, 22);
             this.btnTest.Name = "btnTest";
-            this.btnTest.Size = new System.Drawing.Size(75, 23);
+            this.btnTest.Size = new System.Drawing.Size(87, 27);
             this.btnTest.TabIndex = 1;
             this.btnTest.Text = "Tester";
             this.btnTest.UseVisualStyleBackColor = true;
+            this.btnTest.Click += new System.EventHandler(this.btnTest_Click);
             // 
             // cmbLettres
             // 
             this.cmbLettres.FormattingEnabled = true;
-            this.cmbLettres.Location = new System.Drawing.Point(56, 19);
+            this.cmbLettres.Location = new System.Drawing.Point(65, 22);
             this.cmbLettres.Name = "cmbLettres";
-            this.cmbLettres.Size = new System.Drawing.Size(121, 21);
+            this.cmbLettres.Size = new System.Drawing.Size(141, 23);
             this.cmbLettres.TabIndex = 0;
+            this.cmbLettres.SelectedIndexChanged += new System.EventHandler(this.cmbLettres_SelectedIndexChanged);
             // 
             // pctPendu
             // 
-            this.pctPendu.Location = new System.Drawing.Point(434, 92);
+            this.pctPendu.Location = new System.Drawing.Point(506, 106);
             this.pctPendu.Name = "pctPendu";
-            this.pctPendu.Size = new System.Drawing.Size(250, 250);
+            this.pctPendu.Size = new System.Drawing.Size(292, 288);
             this.pctPendu.TabIndex = 2;
             this.pctPendu.TabStop = false;
             // 
             // btnRejouer
             // 
             this.btnRejouer.Image = global::JeuDuPendu.Properties.Resources.refresh_6711474;
-            this.btnRejouer.Location = new System.Drawing.Point(353, 284);
+            this.btnRejouer.Location = new System.Drawing.Point(412, 327);
             this.btnRejouer.Name = "btnRejouer";
-            this.btnRejouer.Size = new System.Drawing.Size(75, 58);
+            this.btnRejouer.Size = new System.Drawing.Size(87, 67);
             this.btnRejouer.TabIndex = 3;
             this.btnRejouer.UseVisualStyleBackColor = true;
+            this.btnRejouer.Click += new System.EventHandler(this.btnRejouer_Click);
+            // 
+            // lblResultat
+            // 
+            this.lblResultat.AutoSize = true;
+            this.lblResultat.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultat.Location = new System.Drawing.Point(122, 374);
+            this.lblResultat.Name = "lblResultat";
+            this.lblResultat.Size = new System.Drawing.Size(69, 20);
+            this.lblResultat.TabIndex = 4;
+            this.lblResultat.Text = "Résultat";
             // 
             // FrmPendu
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(933, 519);
+            this.Controls.Add(this.lblResultat);
             this.Controls.Add(this.btnRejouer);
             this.Controls.Add(this.pctPendu);
             this.Controls.Add(this.grpTest);
             this.Controls.Add(this.grpMot);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.Name = "FrmPendu";
             this.Text = "Jeu du pendu";
             this.Load += new System.EventHandler(this.Pendu_Load);
@@ -133,6 +150,7 @@ namespace JeuDuPendu
             this.grpTest.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pctPendu)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -146,6 +164,7 @@ namespace JeuDuPendu
         private System.Windows.Forms.ComboBox cmbLettres;
         private System.Windows.Forms.PictureBox pctPendu;
         private System.Windows.Forms.Button btnRejouer;
+        private System.Windows.Forms.Label lblResultat;
     }
 }
 
--- JeuDuPendu/JeuDuPendu/FrmPendu.cs
+++ JeuDuPendu/JeuDuPendu/FrmPendu.cs
@@ -32,7 +32,105 @@ namespace JeuDuPendu
         // S'execute au chargement
         private void Pendu_Load(object sender, EventArgs e)
         {
-            RemplirCmbLettres();
+            PreparationPhase1();
+        }
+
+        // Préparation de la phase 1 (phase 1 = saisie du mot a chercher)
+        private void PreparationPhase1()
+        {
+            grpTest.Enabled = false; // Désactivation de la zone de teste
+            lblLettres.Text = ""; // Vide le label des lettres téstées
+            lblResultat.Text = ""; // Vide le label qui affiche le résultat
+            grpMot.Enabled = true; // Activation de la zone de saisie
+            txtMot.Text = ""; // Vide la zone de saisie
+            txtMot.Focus(); // Se positionne sur la zone de saisie
+            txtMot.MaxLength = 15; // Limite la longeur du mot a trouvé
+        }
+
+        // Préparation de la phase 2 (phase 2 = test)
+        private void PreparationPhase2()
+        {
+            grpMot.Enabled = false; // Désactivation de la zone de saisie
+            grpTest.Enabled = true; // Activation de la zone de test
+            RemplirCmbLettres(); // Remplissage du combo avec les lettres de l'alphabet
+            btnTest.Focus(); // Focus sur le bouton de test
+            cmbLettres.SelectedIndex = 0;
+        }
+
+        private void btnRejouer_Click(object sender, EventArgs e)
+        {
+            PreparationPhase1();
+        }
+
+        // Vérification que le mot saisi est constitué uniquement de caractère
+        private bool MotCorrect(string unMot)
+        {
+            unMot = unMot.ToUpper(); // Transforme chaque lettre du mot en majuscule
+            bool correct = true; // Déclaration et initialisatin de la variable correct
+
+            // Boucle sur chaque lettre afin de verifier
+            for(int k = 0; k < unMot.Length; k++)
+            {
+                // Verification que le mot est constitué de l'alphabet (entre A et Z)
+                if(char.Parse(unMot.Substring(k,1)) < 'A' || char.Parse(unMot.Substring(k,1)) > 'Z')
+                {
+                    correct = false;
+                }
+            }
+            return correct;
+        }
+
+        // Evenement : choix d'une lettre dans le menu déroulant
+        private void cmbLettres_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            btnTest.Focus(); // Focus sur le bouton de test
+        }
+
+        // Evenement :  validation du mot saisie
+        private void txtMot_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            // Verifie que la touche Enter a été préssée
+            if (e.KeyChar == (char)Keys.Enter) 
+            {
+                // Verification qu'un mot a été saisie et qu'il soit correct
+                if(!txtMot.Text.Equals("") && MotCorrect(txtMot.Text))
+                {
+                    PreparationPhase2(); // Préparation des objets graphiques pour la phase 2
+                }
+                else
+                {
+                    // Si la zone de saisi est vide
+                    if(txtMot.Text == "")
+                    {
+                        MessageBox.Show("Vous devez saisir un mot !!");
+                        txtMot.Focus();
+                    }
+                    else // Si le mot entré contien un caractère autre que l'alphabet
+                    {
+                        MessageBox.Show("Le moit doit contenir uniquement des lettres alphabétiques");
+                        txtMot.Text = "";
+                        txtMot.Focus();
+                    }
+                }
+            }
+        }
+
+        // Evenement : clique sur le bouton de test
+        private void btnTest_Click(object sender, EventArgs e)
+        {
+            
+            try
+            {
+                char lettre = char.Parse(cmbLettres.SelectedItem.ToString()); // Recupère dans la variable "lettre" la lettre séléctionnée
+                lblLettres.Text = lblLettres.Text + lettre; // Ajout de la lettre téstée dans le label de lettres dèja téstées
+                cmbLettres.Items.Remove(lettre); // Suppression de la lettre séléctionnée du combo
+                cmbLettres.SelectedIndex = 0; // se positonne automatiquement sur la premier lettre présente
+            }
+            catch // Dans le cas ou il y'a tantative de supprimer une lettre alors que le combo est vide
+            {
+                grpTest.Enabled = false;
+            }
+            
         }
     }
 }
